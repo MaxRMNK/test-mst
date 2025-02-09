20b8d47480f65b484587128fbd587845f88d143d
MaxRMNK <115977436+MaxRMNK@users.noreply.github.com>
2025-02-09 20:09:35 +0500

    feat: before edit Hero and Header section

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+// });
+
+const page = document.querySelector('.page');
+
+const header = page.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 10) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
+
